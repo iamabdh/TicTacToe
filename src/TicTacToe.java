de973537cc181e93ee1560cce91cbdf083ff427d
iamabdh <abdullahalbadri09@gmail.com>
2022-12-12 13:09:44 +0400

    initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Abdullah Salim 
+ *
+ */
+public class TicTacToe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
